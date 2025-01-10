b9d239774c704be2c12e85262f84f1929ce9b9a6
gustavojleal <gustavojleal@gmail.com>
2025-01-10 15:51:59 -0500

    T en prod

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import underconstruction from "../public/underconstruction.png";
 function Home() {
   return (
     <div>
+      <h1>Home Page</h1>
+      <h1>`${NODE_ENV}`</h1>
       <Image src={underconstruction} alt="Under Construction" />
     </div>);
 }
