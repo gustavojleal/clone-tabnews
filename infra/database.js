3e193185fdcba772c803d21f2e3ac27763f53b06
gustavojleal <gustavojleal@gmail.com>
2025-01-23 15:52:21 -0500

    To update migrations

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,8 @@
 import { Client } from "pg";
 async function query(queryObject) {
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT,
-        ssl: getSSLValues(),
-        
-    });
+    let client;
     try {
-        await client.connect();
+        client = await getNewClient();
         const result = await client.query(queryObject);
         return result;
     } catch (error) {
@@ -23,19 +15,35 @@ async function query(queryObject) {
 
 }
 
+
+async function getNewClient() {
+   
+    const client = new Client({
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+        port: process.env.POSTGRES_PORT,
+        ssl: getSSLValues(),
+        
+    });
+    await client.connect();
+    return client;
+}
 export default {
-    query: query,
+    query,
+    getNewClient,
 };
 
 function getSSLValues() {
     if (process.env.POSTGRES_CA_CERT && process.env.POSTGRES_CLIENT_CERT && process.env.POSTGRES_CLIENT_KEY) {
-        // Do not sent the actual values to the repositorie
+      
         return ({        
             ca: "process.env.POSTGRES_CA_CERT",
             cert: "process.env.POSTGRES_CLIENT_CERT",
             key: "process.env.POSTGRES_CLIENT_KEY",
         })
     }
-    return process.env.NODE_ENV === "development" ? false : true;
+    return process.env.NODE_ENV === "production" ? true : false;
 }
 
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,8 +1,18 @@
+import database from "infra/database"
+
+
+async function clearDatabase() {
+    await database.query("drop schema public cascade; create schema public;")
+}
+
+beforeAll(clearDatabase)
+
 test("Get to /api/v1/migrations should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/migrations")
+    const response = await fetch("http://localhost:3000/api/v1/migrations", { method: "GET" })
     expect(response.status).toBe(200)
     
-   const responseBody = await response.json()
-
+    const responseBody = await response.json()
     expect(Array.isArray(responseBody)).toEqual(true)
+
+    expect(responseBody.length).toBeGreaterThan(0)
 })
